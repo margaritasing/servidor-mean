519cb7b347abc46022d7ca5364267229648a8304
margaritasing <libiamfreitesm@gmail.com>
2022-05-17 17:38:48 -0300

    agrego el jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 
 const usuarioModel = require("../models/User.js")
 
@@ -12,19 +13,29 @@ const registerUsuario = async (req, res) =>{
            return res.status(501).json({success:false, msg: "El correo ya existe"})           
        }
        const nuevoUsuario = new usuarioModel({ email, password, username, lastname, imageUser })
-
        //implementacion de el encriptado de la contraseña 
        const passwordHash = bcryptjs.hashSync(password, 10);
        nuevoUsuario.password = passwordHash;
        
+       await nuevoUsuario.save();
 
-       await nuevoUsuario.save()
+       // le paso el jwt para generar el token
+       const payload = {
+           id: nuevoUsuario.id
+       }
 
-       res.json({
-           ok: true,
-           email, passwordHash, username, lastname, imageUser,
-           msg: "Usuario registrado"
-       })        
+       
+       jwt.sign(payload, process.env.SECRETA_PALABRA, {expiresIn:60*60*24}, (error, token)=>{
+           res.json({
+               ok: true,
+               id: nuevoUsuario.id,             
+               username, 
+               lastname,              
+               msg: "Usuario registrado",
+               token
+           });        
+       });
+
     } catch (error) {
         res.json({
             ok: false,            
@@ -33,6 +44,7 @@ const registerUsuario = async (req, res) =>{
     }
 
 }
+
 
 
 

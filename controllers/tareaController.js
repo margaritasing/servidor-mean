354157c019bd4a6ab7e6894965c544674ad2b996
margaritasing <libiamfreitesm@gmail.com>
2022-05-19 12:34:56 -0300

    agrego delete

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,6 +1,6 @@
 const Tarea = require("../models/tarea.js")
 
-const createTarea =  async (req, res) =>{
+const createTarea =  async (req, res) => {
 
     const {nombre, descripcion} = req.body
     const id = req.uid
@@ -10,29 +10,65 @@ const createTarea =  async (req, res) =>{
         descripcion,
         creator:id
     })
-
     await nuevaTarea.save()    
-    res.status(200).json({ok: true, msg:"tarea creada", nuevaTarea})
-}
+    res.status(200).json({ok: true, msg:"Tarea creada", nuevaTarea})
+};
 
-const readTarea = async (req, res) =>{
+const readTarea = async (req, res) => {
     const id = req.uid
-
     try {
         const tareas = await Tarea.find({creator:id}).sort({createAt:-1})
-        res.json({
-            ok:true, tareas
-        })
-        
+        return res.json({
+            ok:true, 
+            tareas
+        })        
     } catch (error) {
-        return res.status(404).json({ok: false, msg:"Tareas no encontradas"})
-        
+        return res.status(404).json({ok: false, msg:"Tarea no encontrada"})        
     }
+};
 
-}
+const updateTarea = async (req, res) => {
+     const {id} = req.params;
+    const {nombre, descripcion} = req.body
+    try {
+
+        const tarea = await Tarea.findByIdAndUpdate(id,{nombre, descripcion}, {new: true})
+       return res.json({
+            ok:true, 
+            msg:"Tarea actualizada",
+            tarea
+        })   
+               
+    } catch (error) {
+        return res.status(404).json({ok: false, msg:"Tarea no actualizada"})        
+    }
+    
+
+};
+
+const deleteTarea = async (req, res) =>{
+    const { id } = req.params;    
+
+    try {
+       const tarea = await Tarea.findByIdAndDelete(id);
+
+       return res.json({
+            ok:true, 
+            msg:"La tarea fue eliminada",
+            tarea,
+        });         
+    } catch (error) {
+        return res.status(404).json({
+            ok: false, 
+            msg:"La tarea no fue borrada",
+        }) 
+    }
+};
 
 
 module.exports = {
     createTarea,
-    readTarea
+    readTarea,
+    updateTarea,
+    deleteTarea
 }
--- routes/tareas.js
+++ routes/tareas.js
@@ -1,13 +1,18 @@
 const {Router} = require("express");
-const { createTarea, readTarea} = require("../controllers/tareaController");
+const { createTarea, readTarea, updateTarea, deleteTarea} = require("../controllers/tareaController");
 const verifyToken = require("../middlewares/verifyToken")
+const { check } = require("express-validator");
 
 const taskRouter = Router();
 
 
-taskRouter.post("/create",[verifyToken],  createTarea);
+taskRouter.post("/create", [check("nombre", "Nombre de la tarea es obligado").not().isEmpty(), verifyToken], createTarea);
 
 taskRouter.get("/read", [verifyToken], readTarea);
+
+taskRouter.put("/update/:id", [check("nombre", "Nombre de la tarea es obligado").not().isEmpty(), verifyToken], updateTarea);
+
+taskRouter.delete("/delete/:id", [verifyToken], deleteTarea);
 
 
 
